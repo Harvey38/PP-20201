7018f447d440e74f95d498e2314d80b9f9c97524
Harv38 <tush38635@gmail.com>
2021-06-12 18:13:56 +0530

    Delete

--- React/React/movies/src/Components/Movies.js
+++ React/React/movies/src/Components/Movies.js
@@ -5,25 +5,66 @@ export default class Movies extends Component {
     {
         super(props);
         this.state={
-            movies:getMovies()
+            movies:getMovies(),
+            currSearchText:'',
+            filterMovies:getMovies()
         }
     }
+    onDelete = (id)=>{
+        let filterMovies = this.state.movies.filter(movieObj=>{
+            return movieObj._id!=id
+        })
+        this.setState({
+            movies:filterMovies
+        })
+    }
+     
+    handleChange=(e)=>{
+        this.setState({currSearchText:e.target.value})
+    }
+
     render() {
         let {movies} = this.state;
         return (
-            <div>
-               {
-                 movies.map(movieObj=>(
-                     <div className='movie-item' key={movieObj._id} >
-                         <span>{movieObj.title}</span>
-                         <span>{movieObj.genre.name}</span>
-                         <span>{movieObj.numberInStock}</span>
-                         <span>{movieObj.dailyRentalRate}</span>
-                         <button>Delete</button>
-                     </div>
-                 ))  
-               }
+            <div className='row'>
+              <div className='col-3'>
+                  <h1>Hello</h1>
+              </div>
+              <div className='col-9'>
+              <input value={this.state.currSearchText} onChange={this.handleChange} type='text'></input>
+              <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Stock</th>
+      <th scope="col">Rate</th>
+    </tr>
+  </thead>
+  <tbody>
+   {
+       movies.map(movieObj=>(
+                <tr scope='row' key={movieObj._id} >
+                    <td></td>
+                    <td>{movieObj.title}</td>
+                    <td>{movieObj.genre.name}</td>
+                    <td>{movieObj.numberInStock}</td>
+                    <td>{movieObj.dailyRentalRate}</td>
+                    <td><button type='button' className='btn btn-danger' onClick={()=>this.onDelete(movieObj._id)}>Delete</button></td>
+                </tr>
+            ))  
+          }
+  </tbody>
+</table>    
+                  
+              </div>  
             </div>
+              
         )
     }
 }
+
+
+// {
+//     
