74339089a8d93ad5ac9369f7b55ecbfea953d69f
Harv38 <tush38635@gmail.com>
2021-06-26 17:50:50 +0530

    React Router

--- React/React/movies/src/App.js
+++ React/React/movies/src/App.js
@@ -1,9 +1,18 @@
 import Movies from './Components/Movies'
-
+import About from './Components/About';
+import Home from './Components/Home';
+import Movie from './Components/Movie';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 function App() {
   return (
   <>
-  <Movies/>
+  <BrowserRouter>
+  <Switch>
+  <Route exact path='/' component={Home}/>
+  <Route path='/movies' component={Movies}/>
+  <Route exact path='/about' component={About}/>
+  </Switch>
+  </BrowserRouter>
   </>
   );
 }
---
+++ React/React/movies/src/Components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Movie() {
+    return (
+        <div>
+            <h1>This is a movie Component</h1>
+        </div>
+    )
+}
+
+export default Movie
